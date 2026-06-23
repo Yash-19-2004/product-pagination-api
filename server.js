61d1fb06a86c5488af66bfcc84bef5a0a91b2bc2
MandeepJas <4nm20is077@nmamit.in>
2026-06-24 02:56:12 +0530

    fix render port issue

--- server.js
+++ server.js
@@ -42,7 +42,9 @@ app.get("/products",async(req,res) => {
 
 });
 
-app.listen(5000,() => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
